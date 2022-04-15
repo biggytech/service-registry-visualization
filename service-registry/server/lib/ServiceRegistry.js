9d0cee18c5d5ccce0b2346f4eb34c5bbde72a815
Neli Harbuzava <ineliharbuzava@gmail.com>
2022-04-15 16:30:49 +0300

    Cleanup services

--- service-registry/server/lib/ServiceRegistry.js
+++ service-registry/server/lib/ServiceRegistry.js
@@ -12,6 +12,7 @@ class ServiceRegistry {
   }
 
   get(name, version) {
+    this.cleanup();
     const services = Object.values(this.services).filter(service => service.name === name && semver.satisfies(service.version, version));
 
     // simulate load balancing
@@ -19,6 +20,7 @@ class ServiceRegistry {
   }
 
   register(name, version, ip, port) {
+    this.cleanup();
     const key = this.constructor.key(name, version, ip, port);
 
     if (!this.services[key]) {
@@ -42,6 +44,16 @@ class ServiceRegistry {
     const key = this.constructor.key(name, version, ip, port);
     delete this.services[key];
     return key;
+  }
+
+  cleanup() {
+    const now = Math.floor(new Date() / 1000);
+    Object.keys(this.services).forEach(key => {
+      if (this.services[key].timestamp + this.timeout < now) {
+        delete this.services[key];
+        this.log.debug(`Removed service ${key}`);
+      }
+    });
   }
 }
 
